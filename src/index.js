2ad8cbd7bac53fa057fb8de6c951771e66992bc9
genarion <genarion>
2022-05-13 01:32:15 +0200

    -

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const fs = require('fs');
 
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
+const { exit } = require('process');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -17,9 +19,15 @@ client.once('ready', () => {
   setInterval(()=>{
 
     if( data['user-id'] && ( Date.now() - data.time ) >= ( 2 * 60 * 60 * 1000 ) ) {
-      let server = client.guilds.cache.get( data['server-id'] );
-      let user = server.members.fetch( data['user-id'] );
-      user.roles.remove( data['role-id'] );
+      client.guilds.fetch( data['server-id'] )
+      .then(
+        server => 
+          server.members.fetch( data['user-id'] )
+            .then(
+              user => user.roles.remove( data['role-id'] )
+            )
+        )
+      
     }
 
   }, 60000)
@@ -32,9 +40,18 @@ client.on('messageCreate', message => {
   console.log(message)
   if ( message.type === 'APPLICATION_COMMAND' && message.interaction.commandName === 'debug' && message.embeds[0].description.includes('Server') ){
 
-    const server = client.guilds.fetch( data['server-id'] );
-    const member = server.members.fetch( message.interaction.user.id );
-    member.roles.add( data['role-id'] );
+    client.guilds.fetch( data['server-id'] )
+      .then(
+        function( guild ) {
+          guild.members.fetch( message.interaction.user.id )
+            .then(
+              function( member ){
+                member.roles.add( data['role-id'] )
+              }
+            )
+        }
+      ).catch( console.error )
+
 
     //change the value in the in-memory object
     data.time = Date.now()
@@ -43,14 +60,15 @@ client.on('messageCreate', message => {
     //Serialize as JSON and Write it to a file
     fs.writeFileSync( __dirname + '/data.json', JSON.stringify( data ));
 
-  }else if( message.type === 'DEFAULT' && message.author.id === '343380089083396107' && message.content.includes('canarias') ){
+  }else if( message.type === 'DEFAULT' && message.author.id === '343380089083396107' && message.content.includes('cfgrole') ){
     console.log('configurando rol')
 
     let tempRolId = message.mentions.roles.keys().next().value
 
-    console.log(tempRolId)
+    console.log(tempRolId, data)
 
     data['role-id'] = tempRolId
+    console.log(data)
     fs.writeFileSync( __dirname + '/data.json', JSON.stringify( data ));
 
   }
